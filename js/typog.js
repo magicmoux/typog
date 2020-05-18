461f64d887897e02a06ba5a40a364e445953bd32
Co_Zax <sushibar2929@gmail.com>
2020-05-19 01:06:24 +0200

    Create typog.js

---
+++ js/typog.js
@@ -0,0 +1,59 @@
+//Class
+class typog {
+    //Get arguments
+    constructor(id, speed) {
+      this.id = id;
+      this.speed = speed;
+    }
+    //Write a string in the current div
+    write(val, offset = 0) {
+      return new Promise((resolve, reject) => {
+        if (offset < val.length) {
+          document.getElementById(this.id).innerHTML += val.charAt(offset);
+          offset++;
+          setTimeout(() => {
+            this.write(val, offset);
+          }, this.speed);
+        }
+        setTimeout(() => {
+          resolve(val);
+          clearTimeout(this);
+        }, this.speed * val.length);
+      });
+    }
+    //Delete string in the current div
+    del(val, offset = 0) {
+      return new Promise((resolve, reject) => {
+        if (offset < val.length) {
+          val = val.substring(0, val.length - 1);
+          document.getElementById(this.id).innerHTML = val;
+          setTimeout(() => {
+            this.del(val, offset);
+          }, this.speed);
+        }
+        setTimeout(() => {
+          resolve(val);
+          clearTimeout(this);
+        }, this.speed * val.length);
+      });
+    }
+    //Infinite string writing/deleting
+    infinite(val, to = 1500, i = 0) {
+        let arr;
+        if (Array.isArray(val)) {
+            if (i >= val.length) {i = 0;}
+            arr = val;
+            val = val[i];
+        }
+        this.write(val).then((key) => {
+            setTimeout(() => {
+                this.del(key).then(() => {
+                    setTimeout(() => {
+                        if(arr != undefined){val = arr}
+                        this.infinite(val, to, i + 1);
+                    }, to);
+                });
+            }, to);
+        });
+    }
+}
